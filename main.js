a98cf63f1382d3ed11530992b719afc2f39c1d3d
Cody Pellom <cpellom@appliedsystems.com>
2019-10-21 09:51:39 -0400

    'monday'

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
-var menuIcon = document.getElementById("menuIcon");
-var menuLinks = document.getElementById("linkSection");
-
-var count = 0;
-menuIcon.onclick =
-    function menuShow() {
-        count++;
-        for (i = 0; i < count; i++) {
-            if (count % 2) {
-                menuLinks.style.opacity = "1";
-            } else {
-                menuLinks.style.opacity = "0";
-            }
-        }
-    }
+var merchImage = document.querySelector('merch-image');
 
